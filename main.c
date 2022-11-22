09887cd5fafc22521dd4fb1d98e61185025fa251
Ana Marcela Rodrigues Silva <amarcela@c1r9p1.42sp.org.br>
2022-11-22 14:10:19 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amarcela <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 13:53:02 by amarcela          #+#    #+#             */
+/*   Updated: 2022/11/22 14:07:42 by amarcela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minilibx/mlx.h"
+#include "printf/ft_printf.h"
+
+int	check_arg(int argc, )
+
+int	main(int argc, char **argv)
+{
+	check_arg(argc, argv);
+	if (argc <= 1)
+		ft_printf("please inform the name of the desired fractal\n");
+	
+}
